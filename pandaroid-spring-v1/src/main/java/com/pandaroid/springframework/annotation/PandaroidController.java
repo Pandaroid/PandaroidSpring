720dca60f53a63e0ed3d85366d03e5a0ad444cc5
Payne Pandaroid Wang <346067742@qq.com>
2020-04-11 01:11:13 +0800

    修正 PandaroidController 注解的 Target 为 TYPE

--- pandaroid-spring-v1/src/main/java/com/pandaroid/springframework/annotation/PandaroidController.java
+++ pandaroid-spring-v1/src/main/java/com/pandaroid/springframework/annotation/PandaroidController.java
@@ -1,13 +1,15 @@
 package com.pandaroid.springframework.annotation;
 
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.lang.annotation.*;
 
 /**
  * 控制器
  * @author pandaroid
  */
-@Target(ElementType.FIELD)
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface PandaroidController {
