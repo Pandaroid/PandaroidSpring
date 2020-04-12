967e8435fd2a47e41a31397d9ba06bd2874e9fac
Payne Pandaroid Wang <346067742@qq.com>
2020-04-12 23:11:00 +0800

    PandaroidBeanDefinition

---
+++ pandaroid-spring-v2/src/main/java/com/pandaroid/springframework/beans/support/PandaroidBeanDefinitionReader.java
@@ -0,0 +1,4 @@
+package com.pandaroid.springframework.beans.support;
+
+public class PandaroidBeanDefinitionReader {
+}
---
+++ pandaroid-spring-v2/src/main/java/com/pandaroid/springframework/context/PandaroidApplicationContext.java
@@ -0,0 +1,4 @@
+package com.pandaroid.springframework.context;
+
+public class PandaroidApplicationContext {
+}
